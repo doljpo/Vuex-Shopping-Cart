68fc9458f82ccc917a070638a077a8950aa3e0c6
dol.jpo <dol.jpo@hotmail.com>
2020-06-21 15:44:52 -0300

    Add component to show the products available

---
+++ src/components/ProductList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">{{ product.title }} - {{ product.price }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+import shop from "@/api/shop";
+
+export default {
+  data() {
+    return {
+      products: []
+    };
+  },
+
+  created() {
+    shop.getProducts(products => {
+      this.products = products;
+    });
+  }
+};
+</script>
