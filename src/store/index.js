d80a3c87a7ce61a68eba7e45dd9f48107ca98d77
dol.jpo <dol.jpo@hotmail.com>
2020-06-21 16:00:12 -0300

    Add Vuex to project

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex);
+
+new Vuex.Store({
+    state: {
+
+    },
+
+    getters: {
+
+    },
+
+    actions: {
+
+    },
+
+    mutations: {
+
+    }
+})
